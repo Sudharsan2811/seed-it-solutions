4dd974bfa5e96d2e6ee1cfa0bc7d6c69ca28a70c
Sudharsan2811 <sudharsanselvaraj2007@gmail.com>
2026-09-20 19:31:08 +0530

    Solve: [Q0.1] Basic_level_0_Datatypes_1 (Medium) - JAVA | SEED-IT

---
+++ Problems/general/q0-1-basic-level-0-datatypes-1/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * Problem: Q0.1 - Basic_level_0_Datatypes_1
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-20
+ * Language: java
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       System.out.print("Let's learn 'C' together with \"SEED\" Team");
+    }
+}
